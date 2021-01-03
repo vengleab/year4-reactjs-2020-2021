b44750ff272af44270cf4ef2f46fa4f115b04af8
So Vengleab <sovengleab@gmail.com>
2021-01-03 22:34:50 +0700

    Week6 add bootstrap

--- first-reactjs/src/App.js
+++ first-reactjs/src/App.js
@@ -1,25 +1,64 @@
-import './App.css';
-import Head from './components/Header'
-import Body from './components/Body'
-import HeaderItem from './components/HeaderItem'
+import "./App.css";
+import Head from "./components/Header";
+import Body from "./components/Body";
+import HeaderItem from "./components/HeaderItem";
+import React from "react";
+import Button from "react-bootstrap/Button";
+// easy manage code, prevent production error
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 // functional component
 // function App() {
 //   return <div>Hello world</div>
 // }
 
-export const App = () => {
-  return (
-    <>
-      <Head>
-        <HeaderItem active>Home</HeaderItem>
-        {/* <HeaderItem label="Home" active/>*/}
-        <HeaderItem>Contact Us</HeaderItem>
-        <HeaderItem>About Us</HeaderItem>
-      </Head>
-      <Body/>
-    </>
-  )
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.emailInputRef = React.createRef();
+  }
+
+  // onload after render
+  componentDidMount() {
+    // alert("On load")
+    this.emailInputRef.current.focus();
+    console.log("emailInputRef", this.emailInputRef.current.value);
+  }
+
+  render() {
+    const val = {
+      active: true,
+      text: "value"
+    };
+    return (
+      <>
+        <Head>
+          <HeaderItem active={val.active} text={val.text}>Home</HeaderItem>
+          <HeaderItem {...val}>Home</HeaderItem>
+          {/* <HeaderItem label="Home" active/>*/}
+          <HeaderItem>Contact Us</HeaderItem>
+          <HeaderItem>About Us</HeaderItem>
+          <Button
+            style={{ float: "right" }}
+            className="float-end"
+            variant="danger"
+          >
+            Login
+          </Button>
+        </Head>
+        {/* <Body/> */}
+        <div style={{ marginTop: "10px" }}>Login</div>
+        <div>
+          <label for="email">Email</label>
+          <input ref={this.emailInputRef} name="email" value="vengleab@g.com" />
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input name="password" />
+        </div>
+      </>
+    );
+  }
 }
 
 // class components
--- first-reactjs/src/components/Header/index.js
+++ first-reactjs/src/components/Header/index.js
@@ -1,10 +1,17 @@
 import "./Header.scss";
 import HeaderItem from "../HeaderItem";
 
+// props = {
+//   children: ""
+// }
+
 const Header = (props) => {
+  const { children ,  isFluid} = props;
+  // const children = props.children; // the same as above
+
   return (
-    <div className="header">
-      {props.children}
+    <div className={`header ${isFluid ? "fluid": ""}`}  >
+      { children }
     </div>
   );
 };
